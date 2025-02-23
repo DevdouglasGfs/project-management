2230610327335094e5aeaff4dddadbfb0f90b024
SHADOW <105404079+DevdouglasGfs@users.noreply.github.com>
2025-02-23 04:37:28 -0300

    configure TypeScript and package settings; add Vue integration and path aliases

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,27 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import path from "node:path";
+import { fileURLToPath } from "url";
+
+import vue from "@astrojs/vue";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src/"),
+        "@pages/*": path.resolve(__dirname, "src/pages/*"),
+        "@atomics/*": path.resolve(__dirname, "src/atomics/*"),
+        "@components/*": path.resolve(__dirname, "src/components/*"),
+        "@layouts/*": path.resolve(__dirname, "src/layouts/*"),
+        "@styles/*": path.resolve(__dirname, "src/assets/css/*"),
+        "@modules/*": path.resolve(__dirname, "src/modules/*"),
+      },
+    },
+  },
+
+  integrations: [vue()],
+});
